f35f1247949cb77f3d9bfa28eff6da583cea2d22
bvaleiro@gmail.com <bvaleiro@gmail.com>
2023-08-25 14:48:47 -0300

    feature:class Category

---
+++ src/main/java/com/bsvcode/dtscatolog/DtscatologApplication.java
@@ -0,0 +1,13 @@
+package com.bsvcode.dtscatolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtscatologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtscatologApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsvcode/dtscatolog/DtscatologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsvcode.dtscatolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtscatologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
