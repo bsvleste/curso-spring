25943df3b64b3461689cef187c0aebddd12c2b96
bvaleiro@gmail.com <bvaleiro@gmail.com>
2023-08-31 18:53:42 -0300

    feature:entity product

--- src/main/java/com/bsvcode/dtscatolog/resources/CategoryResource.java
+++ src/main/java/com/bsvcode/dtscatolog/resources/CategoryResource.java
@@ -3,7 +3,6 @@ package com.bsvcode.dtscatolog.resources;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
